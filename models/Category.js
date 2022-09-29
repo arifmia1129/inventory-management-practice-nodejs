0880de02850cd8ff0d0ca1a6e6debfd43c045b1b
arifvtti <arif.vtti@gmail.com>
2022-09-29 15:08:49 +0600

    create category model

---
+++ models/Category.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
+
+
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "Please provide a category name"],
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    description: String,
+    imageUrl: {
+        type: String,
+        validate: [validator.isURL, "Please provide a valid image URL"]
+    }
+}, {
+    timestamps: true
+})
+
+
+const Category = mongoose.model("Category", categorySchema);
+
+exports = Category;
